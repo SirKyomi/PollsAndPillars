9b7f405f835e58a5a6e341540a3a581a7321b24d
LeutnantJohn <jonathan.konrad@t-online.de>
2024-06-22 10:03:36 +0200

    Buildable tiles detected

--- Game/Logic/PollsAndPillars.cs
+++ Game/Logic/PollsAndPillars.cs
@@ -3,14 +3,30 @@ using System;
 
 public partial class PollsAndPillars : Node2D
 {
+	TileMap tileMap;
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		GD.Print("Test");
+		tileMap = GetNode<TileMap>("TileMap");
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+		if (Input.IsActionJustPressed("click"))
+		{
+			GD.Print("click");
+			var mousePosition = GetGlobalMousePosition();
+			GD.Print("MousePosition " + mousePosition);
+
+			var tileMousePosition = tileMap.LocalToMap(mousePosition);
+			GD.Print("TileMousePosition " + tileMousePosition);
+
+			var x = tileMap.GetCellAtlasCoords(1, tileMousePosition);
+			GD.Print(x);
+			if (x == new Vector2I(6, 1)){
+				GD.Print("Buildable Tile found");
+			}
+		}
 	}
 }
