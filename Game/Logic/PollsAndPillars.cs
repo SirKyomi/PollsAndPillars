f0102b3632c596dc941c67667c62cf05b209bb8d
LeutnantJohn <jonathan.konrad@t-online.de>
2024-06-22 15:32:13 +0200

    Logic Place Items

--- Game/Logic/PollsAndPillars.cs
+++ Game/Logic/PollsAndPillars.cs
@@ -4,10 +4,16 @@ using System;
 public partial class PollsAndPillars : Node2D
 {
 	TileMap tileMap;
+	PopupMenu popupMenu;
+	int groundLayer = 2;
+	bool popupIsShowing = false;
+	bool mouseIsInPopup = false;
+	Vector2I mousePositionSuccessFullClick;
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
 		tileMap = GetNode<TileMap>("TileMap");
+		popupMenu = GetNode<PopupMenu>("PopupMenu");
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -15,18 +21,41 @@ public partial class PollsAndPillars : Node2D
 	{
 		if (Input.IsActionJustPressed("click"))
 		{
-			GD.Print("click");
 			var mousePosition = GetGlobalMousePosition();
-			GD.Print("MousePosition " + mousePosition);
-
 			var tileMousePosition = tileMap.LocalToMap(mousePosition);
-			GD.Print("TileMousePosition " + tileMousePosition);
-
-			var x = tileMap.GetCellAtlasCoords(1, tileMousePosition);
-			GD.Print(x);
-			if (x == new Vector2I(6, 1)){
-				GD.Print("Buildable Tile found");
+			var atlasCoordsMouse = tileMap.GetCellAtlasCoords(1, tileMousePosition);
+			
+			if (atlasCoordsMouse == new Vector2I(6, 1) && !(popupIsShowing && mouseIsInPopup)) {
+				//GD.Print("Buildable Tile found and accessible");
+				var absoluteMousePosition = GetViewport().GetMousePosition();
+				popupMenu.Popup(new Rect2I((int)absoluteMousePosition.X, (int)absoluteMousePosition.Y, popupMenu.Size.X, popupMenu.Size.Y));
+				popupIsShowing = true;
+				mousePositionSuccessFullClick = tileMousePosition;
 			}
 		}
+	}
+
+	private void OnPopupMenuIdPressed(int id) {
+		var sourceID = 1;
+		var atlasCoordsTargetTile = id == 1 ? new Vector2I(2, 4) : new Vector2I(1, 4);
+		tileMap.SetCell(groundLayer, mousePositionSuccessFullClick, sourceID, atlasCoordsTargetTile);
+		
+		if (atlasCoordsTargetTile == new Vector2I(2, 4)){
+			//klimafreundlich
+			//stats anpassen
+		} else {
+			//klimaschädlich
+			//stats anpassen
+		}
+
+		popupIsShowing = false;
+	}
+
+	private void OnPopupMenuMouseEntered() {
+		mouseIsInPopup = popupIsShowing;
+	}
+
+	private void OnPopupMenuMouseExited() {
+		mouseIsInPopup = false;
 	}
 }
